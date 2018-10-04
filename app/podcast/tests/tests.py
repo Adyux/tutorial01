6f0301746736385e7cc9c8884a23e54edfa48560
Anudeep Samaiya <anudeepsamaiya@gmail.com>
2018-10-04 12:20:43 +0530

    added tests for podcast model

--- app/podcast/tests/tests.py
+++ app/podcast/tests/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from mixer.backend.django import mixer
+from ..models import Podcast
+
+pytestmark = pytest.mark.django_db
+
+class TestModels(object):
+
+    def test_podcast_model(self, **kwargs):
+        test_model = mixer.blend(Podcast, host=None, name='test')
+        assert Podcast.objects.filter(id=test_model.id).count() == 1
